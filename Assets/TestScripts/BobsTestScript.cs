2f7588b4a073343faa780cd0aa89f5dcc02a24b2
Robert Bowen <ykantbobreed@hotmail.com>
2014-01-22 15:26:36 -0700

    Added Switch, animCTRL, and inspector controller....
    Change the variable in the script section of the inspector to see the
    interface animate.

---
+++ Assets/TestScripts/BobsTestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobsTestScript : MonoBehaviour {
+
+	public float Switched = 10.0f;
+	protected Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.SetFloat("Switch L_R", 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat("Switch L_R", ((Switched) / 20));
+	}
+}
