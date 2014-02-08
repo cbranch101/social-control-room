772633fa2fb116a07a7aa94adbfbbdd92fee6e2d
cbranch101 <clay@movementstrategy.com>
2014-02-08 07:41:49 -0500

    Testing a commit from the PC

---
+++ Assets/TestScripts/DebugMouseGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMouseGUI : MonoBehaviour {
+
+	public Texture mouseIndicator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		GUILayout.BeginArea (new Rect(0, 0, 100, 100));
+			GUILayout.Box (webImage);
+		Debug.Log("test");
+
+	}
+}
