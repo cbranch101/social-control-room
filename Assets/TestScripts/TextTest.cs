51757282d17fece950065c9bd9f63f1319c0b109
Clay Branch <clay@movementstrategy.com>
2014-02-25 07:44:53 -0500

    added scene with textBox

---
+++ Assets/TestScripts/TextTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTest : MonoBehaviour {
+
+	CCText textMesh;
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<CCText>();
+		textMesh.Text = "Did you miss the premiere of the Bonnie & Clyde Movie Event? Fear not, catch the Barrow Gang online for free! \r\n\r\nPart 1: http://aetv.us/IB2U9X\r\nPart 2: http://aetv.us/1jIwvg4";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
