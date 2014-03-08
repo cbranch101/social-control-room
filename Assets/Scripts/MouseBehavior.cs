58629ec79c52af0f1f93a88c4104ca1cb15687cc
Clay Branch <clay@movementstrategy.com>
2014-03-08 12:01:41 -0500

    set up first unit test, starting to script mouse behavior

---
+++ Assets/Scripts/MouseBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseBehavior : MonoBehaviour {
+
+	private MouseOverTarget previousMouseOverTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		detectMouseOver();
+	}
+
+	void detectMouseOver() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast (ray, out hit, 1000f)) {
+			MouseOverTarget mouseOverTarget = hit.collider.gameObject.GetComponent<MouseOverTarget>();
+			previousMouseOverTarget = mouseOverTarget;
+			if(mouseOverTarget != null) {
+				mouseOverTarget.isMousedOver = true;
+			}
+		} else {
+			if(previousMouseOverTarget != null) {
+				previousMouseOverTarget.isMousedOver = false;
+				previousMouseOverTarget = null;
+			}
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/MouseOverTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseOverTarget : MonoBehaviour {
+
+	public bool isMousedOver = false;
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
